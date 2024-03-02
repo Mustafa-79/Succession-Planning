18040ade6a256fdeae815ff503b817c30fdb18ac
a1b2d3357 <abdullahghani581@gmail.com>
2024-03-02 13:04:29 +0500

    employee functions done

--- Proj/client/src/pages/Dashboard.jsx
+++ Proj/client/src/pages/Dashboard.jsx
@@ -1,7 +1,7 @@
 import React, { useContext, useState } from 'react';
 import { UserContext } from '../../context/userContext';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHouse, faFileArrowDown, faFileArrowUp, faStreetView, faGear, faBuilding, faUser } from '@fortawesome/free-solid-svg-icons';
+import { faHouse, faFileArrowDown, faFileArrowUp, faStreetView, faGear, faBuilding, faUser,faFileLines,faTriangleExclamation,faEye } from '@fortawesome/free-solid-svg-icons';
 import './Dashboard.css'; // Assuming you have a CSS file for additional styling
 import './fonts.css'; // Import the CSS file where you imported the font
 
@@ -50,7 +50,39 @@ export default function Dashboard() {
                         <FontAwesomeIcon icon={faUser} size='xl' color='rgb(196,196,202)' />
                         <a href="">{user.name}</a>
                     </div>
-                    <div className='employeeFunctions'></div>
+                    <div className='employeeFunctions'>
+                      <div className='employeeFunction'>
+                        <div className='func'>High Potential Employees</div>
+                        <div className='countAndView'>
+                          <div className='funcCount'>06</div>
+                          <div className='iconAndView'>
+                            <FontAwesomeIcon icon={faFileLines} size='3x' color='rgb(255,157,71)' />
+                            <a href="">View</a>
+                          </div>
+                        </div>
+                      </div>
+                      <div className='employeeFunction'>
+                        <div className='func'>Total Employees</div>
+                        <div className='countAndView'>
+                          <div className='funcCount'>107</div>
+                          <div className='iconAndView'>
+                            <FontAwesomeIcon icon={faEye} size='3x' color='rgb(255,157,71)' />
+                            <a href="">View</a>
+                          </div>
+                        </div>
+                      </div>
+                      <div className='employeeFunction'>
+                        <div className='func'>Employees at Risk</div>
+                        <div className='countAndView'>
+                          <div className='funcCount'>12</div>
+                          <div className='iconAndView'>
+                          <FontAwesomeIcon icon={faTriangleExclamation} size='3x' color='rgb(255,157,71)'/>
+                          <a href="">View</a>
+                          </div>
+                        </div>
+                      </div>
+
+                    </div>
                     <div className='employeeSection'>
                         <div className='searchAndAdd'></div>
                         <div className='employeeData'></div>
