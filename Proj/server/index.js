ab9f9a1e18de29935010888879921f639643c85b
Mustafa-79 <25100079@lums.edu.pk>
2024-04-17 18:34:52 +0500

    Update index.js

--- Proj/server/index.js
+++ Proj/server/index.js
@@ -24,16 +24,17 @@ init_weights(db);
 
 
 // middleware
+// Allow requests from http://localhost:5173 and include credentials
+app.use(cors({
+    origin: true,
+    credentials: true,
+  }));
 // app.use(express.json())
 app.use(cookieParser())
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }));
 
 // Allow requests from http://localhost:5173
-app.use(cors({
-    origin: 'http://localhost:5173',
-  }));
-
 
 app.use('/', require('./routes/authRoutes'))
 
